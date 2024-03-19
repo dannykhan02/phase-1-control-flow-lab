911f06ec9314cb259c17b5358fbe3cab4212269e
dan <dannykhan614@gmail.com>
2024-03-19 23:17:26 +0300

    complete flow

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 199) {
+      return 'This one is on me!';
+  } else if (feet >= 400 && feet <= 2000) {
+      return 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+      return 'No can do.';
+  } else {
+      return 'Invalid input.';
+  }
+}
+  
+function ternaryCheckCity(city){
+ return city === 'NYC' ? "Ok, sounds good." : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case'generous':
+          return 'Thank you so much.';
+      case'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
